c2ef31cbaad44109cdb56b3b6b127a4d7267312c
lovesworking <austinlovesworking@gmail.com>
2025-09-21 15:46:39 -0600

    chore: export settings api from core

--- packages/devtools-floating-menu/src/index.tsx
+++ packages/devtools-floating-menu/src/index.tsx
@@ -4,6 +4,11 @@ import { View, Text, StyleSheet } from "react-native";
 // Export FloatingMenu and its types
 export { FloatingMenu } from "./floatingMenu/FloatingMenu";
 export * from "./floatingMenu/types";
+export {
+  DevToolsSettingsModal,
+  useDevToolsSettings,
+} from "./floatingMenu/DevToolsSettingsModal";
+export type { DevToolsSettings } from "./floatingMenu/DevToolsSettingsModal";
 
 // Export AppHost components
 export {
